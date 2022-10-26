90bfd6e20c08ac85393048c63c44ed1c3e833884
ludmilact <ludmilatimoteo@Outlook.com>
2022-10-26 11:49:03 -0300

    Configuracao inicial projeto.
    Configuração projeto com Spring.
    Criação das pastas webapp, WEB-INF, jsp e da classe main.jsp
    Configuração do pom.xml
    Adição das dependências Tomcat Embed Jasper e JSTL.
    Criação da classe main.
    Criação do primeiro controller.
    Configuração git ignore.

---
+++ src/main/java/com/empiricus/statusviajante/StatusviajanteApplication.java
@@ -0,0 +1,13 @@
+package com.empiricus.statusviajante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusviajanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusviajanteApplication.class, args);
+	}
+
+}
