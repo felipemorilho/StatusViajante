c7761c8080679b99da440475769c2be4077c350f
ludmilact <ludmilatimoteo@Outlook.com>
2022-10-31 15:58:50 -0300

    controller para testes

--- src/main/java/com/empiricus/statusviajante/controller/FirstController.java
+++ src/main/java/com/empiricus/statusviajante/controller/FirstController.java
@@ -3,17 +3,18 @@ package com.empiricus.statusviajante.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.processing.Generated;
 
-@Controller
+@RequestMapping("/usuario")
+@RestController
 public class FirstController {
 
     @GetMapping({"/", "/main"})
-    public String main(@RequestParam(value =  "name", defaultValue = "world...Spring", required = true)
-                           String name, Model model) {
-        model.addAttribute("name", name);
-        return "main";
+    public String hello(){
+        return "olá, mundo";
     }
 }
