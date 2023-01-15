50c7be64a53fdd219862c22d89be2be3635dffc5
Felipe Morilho <felipemdecastro@gmail.com>
2023-01-15 20:09:38 -0300

    Alterando a CategoriaGastoController para Kotlin

--- src/main/java/com/empiricus/statusviajante/controller/CategoriaGastoController.kt
+++ src/main/java/com/empiricus/statusviajante/controller/CategoriaGastoController.kt
@@ -1,64 +1,60 @@
-package com.empiricus.statusviajante.controller;
+package com.empiricus.statusviajante.controller
 
-import com.empiricus.statusviajante.model.CategoriaGastoModel;
-import com.empiricus.statusviajante.repository.CategoriaGastoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.empiricus.statusviajante.model.CategoriaGastoModel
+import com.empiricus.statusviajante.repository.CategoriaGastoRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.function.Function
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/categoria")
-public class CategoriaGastoController {
-
+class CategoriaGastoController {
     @Autowired
-
-    private CategoriaGastoRepository categoriaGastoRepository;
-
+    private val categoriaGastoRepository: CategoriaGastoRepository? = null
     @GetMapping
-    public ResponseEntity<List<CategoriaGastoModel>> GetAll() {
-        return ResponseEntity.ok(categoriaGastoRepository.findAll());
-
+    fun GetAll(): ResponseEntity<List<CategoriaGastoModel?>> {
+        return ResponseEntity.ok(categoriaGastoRepository!!.findAll())
     }
 
     @GetMapping("/{idCategoria}")
-    public ResponseEntity<CategoriaGastoModel> GetById(@PathVariable Long idCategoria) {
-
-        return categoriaGastoRepository.findById(idCategoria)
-
-
-                .map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    fun GetById(@PathVariable idCategoria: Long): ResponseEntity<CategoriaGastoModel> {
+        return categoriaGastoRepository!!.findById(idCategoria)
+            .map<ResponseEntity<CategoriaGastoModel>>(Function { resp: CategoriaGastoModel? -> ResponseEntity.ok(resp) })
+            .orElse(ResponseEntity.notFound().build())
     }
 
     @GetMapping("/nome/{nome}")
-    public ResponseEntity<List<CategoriaGastoModel>> GetByNome(@PathVariable String nome) {
-        return ResponseEntity.ok(categoriaGastoRepository.findAllByNomeContainingIgnoreCase(nome));
+    fun GetByNome(@PathVariable nome: String?): ResponseEntity<List<CategoriaGastoModel>> {
+        return ResponseEntity.ok(
+            categoriaGastoRepository!!.findAllByNomeContainingIgnoreCase(
+                nome!!
+            )
+        )
     }
 
     @PostMapping
-    public ResponseEntity post(@RequestBody CategoriaGastoModel categoriaGastoModel) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(categoriaGastoRepository.save(categoriaGastoModel));
-        } catch(Exception exception){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    fun post(@RequestBody categoriaGastoModel: CategoriaGastoModel): ResponseEntity<*> {
+        return try {
+            ResponseEntity.status(HttpStatus.CREATED).body(categoriaGastoRepository!!.save(categoriaGastoModel))
+        } catch (exception: Exception) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.message)
         }
     }
 
     @PutMapping
-    public ResponseEntity put(@RequestBody CategoriaGastoModel categoriaGastoModel) {
-        try {
-            return ResponseEntity.ok(categoriaGastoRepository.save(categoriaGastoModel));
-        } catch(Exception exception){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    fun put(@RequestBody categoriaGastoModel: CategoriaGastoModel): ResponseEntity<*> {
+        return try {
+            ResponseEntity.ok(categoriaGastoRepository!!.save(categoriaGastoModel))
+        } catch (exception: Exception) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.message)
         }
     }
 
     @DeleteMapping("/{idCategoria}")
-    public void Delete(@PathVariable Long idCategoria) {
-        categoriaGastoRepository.deleteById(idCategoria);
+    fun Delete(@PathVariable idCategoria: Long) {
+        categoriaGastoRepository!!.deleteById(idCategoria)
     }
 }
-
