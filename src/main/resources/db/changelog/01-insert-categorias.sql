7080fe0949ef37968226a038cd7d4bb3bebd561e
ludmilact <ludmilatimoteo@Outlook.com>
2023-02-04 17:15:05 -0300

    Criação liquibase para gerar categorias fixas no banco de dados.

---
+++ src/main/resources/db/changelog/01-insert-categorias.sql
@@ -0,0 +1,16 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:1
+
+INSERT INTO categoria (nome) VALUES ('Alimentação');
+INSERT INTO categoria (nome) VALUES ('Saúde');
+INSERT INTO categoria (nome) VALUES ('Lazer');
+INSERT INTO categoria (nome) VALUES ('Transporte');
+INSERT INTO categoria (nome) VALUES ('Estadia');
+INSERT INTO categoria (nome) VALUES ('Atividades');
+INSERT INTO categoria (nome) VALUES ('Lavanderia');
+INSERT INTO categoria (nome) VALUES ('Compras');
+INSERT INTO categoria (nome) VALUES ('Celular');
+INSERT INTO categoria (nome) VALUES ('Documentação');
+INSERT INTO categoria (nome) VALUES ('Outros');
+
